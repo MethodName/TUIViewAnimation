ab536d3e2af31a236d37a622c8759113d39b6afc
code <methodname@qq.com>
2015-10-20 22:31:30 +0800

    Initial Commit

---
+++ UIView_Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIView_Animation
+//
+//  Created by tangmingming on 15/10/20.
+//  Copyright (c) 2015年 tangmingming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIView_AnimationTests/UIView_AnimationTests.m
@@ -0,0 +1,40 @@
+//
+//  UIView_AnimationTests.m
+//  UIView_AnimationTests
+//
+//  Created by tangmingming on 15/10/20.
+//  Copyright (c) 2015年 tangmingming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface UIView_AnimationTests : XCTestCase
+
+@end
+
+@implementation UIView_AnimationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
